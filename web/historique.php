02fd098e9463eb21763f5cf5135d97952cc0708a
BananeRotative <alexis.rochon--sanz@isen-ouest.yncrea.fr>
2024-12-17 17:20:45 +0100

    Navbar (se déconnecter est mal placé)

--- web/historique.php
+++ web/historique.php
@@ -2,7 +2,7 @@
 session_start();
 include_once('database.php');
 if (! isset($_SESSION["user_id"])) {
-    header("location:login.php?msg=WRONG");
+    header("location:login.php");
     exit;
 }
 ?>
@@ -12,8 +12,14 @@ if (! isset($_SESSION["user_id"])) {
     <head>
         <meta charset="utf-8">
         <title> Historique des RDV </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
     <body>
-        
+        <nav class="navbar bg-primary navbar-expand-lg">
+            <img src="img/logo_small.png" srcset="img/logo_medium 900w, img/logo_large.png 1900w, img/logo_xlarge.png 3000w" alt="Logo">
+            <form method="get" action="request.php">
+                <button class="btn border-light rounded-5 text-light" type="submit" value="logout">Se déconnecter</button>
+            </form>
+        </nav>
     </body>
 </html>
